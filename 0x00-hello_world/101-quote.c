ca6b385e25c0580bde5b8c7e48df4a061b4ba367
avispro20 <solomonok07@gmail.com>
2022-07-02 05:41:36 -0700

    Updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-* main -prints exactly "and that piece of art is usefule"
+* main - prints exactly "and that piece of art is useful"
 * - Dora Korper , 2015-10-19",
 * followed by a new line, to the standard error.
 * Return: Always 0 (Success)
