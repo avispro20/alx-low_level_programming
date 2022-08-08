71ebb4f5167927ffdb4ee02d85c56e5f94cb9262
avispro20 <solomonok07@gmail.com>
2022-08-08 09:09:27 -0700

    Function that prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,26 +1,13 @@
-#include "variadic_functions.h"
+#include "function_pointers.h"
 
 /**
- * sum_them_all - returns the sum of all its parameters.
- * @n: amount of the arguments.
- *
- * Return: sum of its parameters.
+ * print_name - prints a name
+ * @name: name to be printed
+ * @f: callback function that prints name
  */
-int sum_them_all(const unsigned int n, ...)
+void print_name(char *name, void (*f)(char *))
 {
-	va_list valist;
-	unsigned int i;
-	int sum = 0;
-
-	if (n == 0)
-		return (0);
-
-	va_start(valist, n);
-
-	for (i = 0; i < n; i++)
-		sum += va_arg(valist, int);
-
-	va_end(valist);
-
-	return (sum);
+	if (!f || !name)
+		return;
+	(*f)(name);
 }
